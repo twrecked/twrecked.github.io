3614479cef0a4d001cf8c443f9500edfb95b7f2a
Steve Herrell <47094394+twrecked@users.noreply.github.com>
2021-02-03 16:38:45 -0500

    Update en.js

--- lang/en.js
+++ lang/en.js
@@ -1,11 +1,27 @@
 
 export var messages = {
 
-	hello: "hello",
-	goodbye: "good bye",
-
+	// image viewer
 	image: {
-		start_stream: 'click to start live stream',
-		stop_stream: 'click to stop live stream'
-	}
+		start_stream: 'view live stream',
+		stop_stream: 'stop live stream'
+	},
+	
+	// library viewer
+	library: {
+		first_page: 'first page',
+		previous_page: 'previous page',
+		next_size: 'next grid size',
+		close: 'close library',
+		next_page: 'next page',
+		last_page: 'last page'
+	},
+
+	// video/stream viewer
+	video: {
+		stop: 'stop',
+		play: 'play',
+		pause: 'pause',
+		fullscreen: 'go full screen'
+	},
 }
