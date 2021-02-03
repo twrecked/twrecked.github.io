0d8778279176a19a84fa6e503be009d268151c03
Steve Herrell <47094394+twrecked@users.noreply.github.com>
2021-02-03 14:21:20 -0500

    Update fr.js

--- lang/fr.js
+++ lang/fr.js
@@ -1,6 +1,11 @@
 
-var messages = {
+export var messages = {
 
 	hello: "bonjour",
 	goodbye: "au revoir",
+
+	image: {
+		start_stream:'cliquez pour démarrer la diffusion en direct'
+		stop_stream: 'cliquez pour arrêter la diffusion en direct'
+	}
 }
